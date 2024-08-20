2c66a6783bdc61077368e8c12f2378f376f64435
jennasimit <jennasimit@gmail.com>
2024-08-20 18:43:57 +0100

    PPsummarise more flexible

--- R/summarise.R
+++ R/summarise.R
@@ -62,66 +62,110 @@ MPPmodGroups <- function(PP1) {
 
 #' @title Summarise PP and MPP results from single-trait fine-mapping and flashfm, by SNP and by SNP group
 #' @param fm.multi output from flashfm function
-#' @param snpGroups  list of two sets of snp groups; output from makeSNPgroups2
-#' @param minPP a single value such that output for groups consists of group models where PP > minPP for at least one set of fine-mapping results; default is 0.05 
+#' @param snpGroups  list of two sets of snp groups; output from makeSNPgroups2; setting snpGroups=NULL will return only SNP-level results
+#' @param minPP a single value such that output for groups consists of group models where PP > minPP for at least one set of fine-mapping results; default is 0.01 
 #' @return a list of 4 objects: MPP lists trait-specific PP of SNP inclusion in a model; MPPg lists trait-specific PP of SNP group inclusion in a model;  
 #'			PP lists trait-specific model PP; PPg lists trait-specific model PP in terms of  SNP group
 #' @author Jenn Asimit
 #' @export
-PPsummarise <- function (fm.multi, snpGroups, minPP=0.05) 
+PPsummarise <- function (fm.multi, snpGroups, minPP=0.01) 
 # for one TOdds setting in fm.multi, snpGroups is a list of 2 snpgroups
 {
 
 	MPP <- fm.multi$MPP
 	pp <- fm.multi$PP	
 	M <- length(pp)
+	
+	if(!is.null(snpGroups)) {
 	snpgroups <- snpGroups[[1]]
 	snpgroups2 <- snpGroups[[2]]
+	}
+	
 	tnames <- names(fm.multi$PP)
 	
 	PPout <- PPout2 <- outPPg <- outPP <- vector("list",M)
 	MPPout <- MPPout2 <- outMPPg <- outMPP <- vector("list",M)
 	
-	for(i in 1:M) {
-	 PPout[[i]] <- PPmodGroups(fm.multi$PP[[i]][,1],snpgroups, minPP=0)
-	 PPout2[[i]] <- PPmodGroups(fm.multi$PP[[i]][,2],snpgroups2, minPP=0)
-	 pplist <- list(t(data.frame(PPout[[i]]$group[,2],row.names=rownames(PPout[[i]]$group))),t(data.frame(PPout2[[i]]$group[,2],row.names=rownames(PPout2[[i]]$group))))
-	 outPPg[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
-	 colnames(outPPg[[i]]) <- fm.multi$sharing
-	 pplist <- list(t(data.frame(PPout[[i]]$snp[,3],row.names=rownames(PPout[[i]]$snp))),t(data.frame(PPout2[[i]]$snp[,3],row.names=rownames(PPout2[[i]]$snp))))
-	 outPP[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
-	 colnames(outPP[[i]]) <- fm.multi$sharing
+	if(!is.null(snpGroups)) {
 	 
-	 MPPout[[i]] <- MPPmodGroups(PPout[[i]]$snp)
-	 MPPout2[[i]] <- MPPmodGroups(PPout2[[i]]$snp)
-	 pplist <- list(t(data.frame(MPPout[[i]][,1],row.names=rownames(MPPout[[i]]))),t(data.frame(MPPout2[[i]][,1],row.names=rownames(MPPout2[[i]]))))
-	 outMPP[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
-	 colnames(outMPP[[i]]) <- fm.multi$sharing
-	 
-	  MPPout[[i]] <- MPPmodGroups(PPout[[i]]$group)
-	  MPPout2[[i]] <- MPPmodGroups(PPout2[[i]]$group)
+	  for(i in 1:M) {
+	  PPout[[i]] <- PPmodGroups(fm.multi$PP[[i]][,1],snpgroups, minPP=0)
+	  PPout2[[i]] <- PPmodGroups(fm.multi$PP[[i]][,2],snpgroups2, minPP=0)
+	  pplist <- list(t(data.frame(PPout[[i]]$group[,2],row.names=rownames(PPout[[i]]$group))),t(data.frame(PPout2[[i]]$group[,2],row.names=rownames(PPout2[[i]]$group))))
+	  outPPg[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
+	  colnames(outPPg[[i]]) <- fm.multi$sharing  
+	  
+	  pplist <- list(t(data.frame(PPout[[i]]$snp[,3],row.names=rownames(PPout[[i]]$snp))),t(data.frame(PPout2[[i]]$snp[,3],row.names=rownames(PPout2[[i]]$snp))))
+	  outPP[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
+	  colnames(outPP[[i]]) <- fm.multi$sharing
+	  
+	  MPPout[[i]] <- MPPmodGroups(PPout[[i]]$snp)
+	  MPPout2[[i]] <- MPPmodGroups(PPout2[[i]]$snp)
 	  pplist <- list(t(data.frame(MPPout[[i]][,1],row.names=rownames(MPPout[[i]]))),t(data.frame(MPPout2[[i]][,1],row.names=rownames(MPPout2[[i]]))))
-	  outMPPg[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
-	  colnames(outMPPg[[i]]) <- fm.multi$sharing
-	  
-	  ind <- which(outPPg[[i]][,1]>minPP | outPPg[[i]][,2]>minPP)
-	  rnames <- rownames(outPPg[[i]])
-	  if(length(ind)>1) {tmp <- outPPg[[i]][ind,]} else{tmp <- t(as.matrix(outPPg[[i]][ind,]))}
-	  rownames(tmp) <- rnames[ind]
-	  outPPg[[i]] <- tmp
-	  
-	  ind <- which(outMPPg[[i]][,1]>minPP | outMPPg[[i]][,2]>minPP)
-	  rnames <- rownames(outMPPg[[i]])
-	  if(length(ind)>1) {tmp <- outMPPg[[i]][ind,]} else{tmp <- t(as.matrix(outMPPg[[i]][ind,]))}
-	  rownames(tmp) <- rnames[ind]
-	  outMPPg[[i]] <- tmp
-	  
-	  }
-	  
-	  
-	 names(outMPPg) <- names(outMPP) <- names(outPP)  <- names(outPPg) <- tnames
+	  outMPP[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
+	  colnames(outMPP[[i]]) <- fm.multi$sharing
 	 
-	return(list(MPP=outMPP, MPPg=outMPPg, PP=outPP, PPg=outPPg)) 
+	   MPPout[[i]] <- MPPmodGroups(PPout[[i]]$group)
+	   MPPout2[[i]] <- MPPmodGroups(PPout2[[i]]$group)
+	   pplist <- list(t(data.frame(MPPout[[i]][,1],row.names=rownames(MPPout[[i]]))),t(data.frame(MPPout2[[i]][,1],row.names=rownames(MPPout2[[i]]))))
+	   outMPPg[[i]] <- t(do.call("smartbind",c(pplist,fill=0)))
+	   colnames(outMPPg[[i]]) <- fm.multi$sharing
+	  
+	   ind <- which(outPPg[[i]][,1]>minPP | outPPg[[i]][,2]>minPP)
+	   rnames <- rownames(outPPg[[i]])
+	   if(length(ind)>1) {tmp <- outPPg[[i]][ind,]} else{tmp <- t(as.matrix(outPPg[[i]][ind,]))}
+	   rownames(tmp) <- rnames[ind]
+	   outPPg[[i]] <- tmp
+	  
+	   ind <- which(outMPPg[[i]][,1]>minPP | outMPPg[[i]][,2]>minPP)
+	   rnames <- rownames(outMPPg[[i]])
+	   if(length(ind)>1) {tmp <- outMPPg[[i]][ind,]} else{tmp <- t(as.matrix(outMPPg[[i]][ind,]))}
+	   rownames(tmp) <- rnames[ind]
+	   outMPPg[[i]] <- tmp
+	   
+	   names(outMPPg) <- names(outPPg) <- tnames
+	   names(outMPP) <- names(outPP)  <- tnames
+	   
+	   }
+	   	   
+	    for(i in 1:M) {
+	    ind <- order(outMPP[[i]][,2],decreasing=TRUE)
+	    outMPP[[i]] <- outMPP[[i]][ind,]
+	   }
+	    for(i in 1:M) {
+	    ind <- order(outPP[[i]][,2],decreasing=TRUE)
+	    outPP[[i]] <- outPP[[i]][ind,]
+	   }
+	   
+	   OUT <- list(MPP=outMPP, MPPg=outMPPg, PP=outPP, PPg=outPPg)
+	    }
+	    	  
+	  	  
+	  if(is.null(snpGroups)){
+	   
+	   outPP <- fm.multi$PP 
+	   for(i in 1:M) outMPP[[i]] <- t(MPP[(2*i-1):(2*i),])  
+	   
+	   for(i in 1:M) {
+	    colnames(outMPP[[i]]) <- fm.multi$sharing
+	    ind <- order(outMPP[[i]][,2],decreasing=TRUE)
+	    outMPP[[i]] <- outMPP[[i]][ind,]
+	   }
+	    for(i in 1:M) {
+	    colnames(outPP[[i]]) <- fm.multi$sharing
+	    ind <- order(outPP[[i]][,2],decreasing=TRUE)
+	    outPP[[i]] <- outPP[[i]][ind,]
+	   }
+	   names(outMPP) <- names(outPP)  <- tnames
+	  
+	    
+	   OUT <- list(MPP=outMPP, PP=outPP)
+    	}
+	
+	
+	return(OUT) 
+
+
 	}
 
 
